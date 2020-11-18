a7ba8303c2e9ee8f11d67c20c9d89af54bffa201
Bordee Jakteerangkul <51290396+Bordee2000@users.noreply.github.com>
2020-11-18 18:12:06 +0700

    add slide models

--- Project/slide.js
+++ Project/slide.js
@@ -1,6 +1,6 @@
-var data = {
-    "name":[ "Audi e-tron", "Audi A1", "Audi A4", "Audi A5", "Audi A6", "Audi A7", "Audi A8", "Audi Q2", "Audi Q3", "Audi Q5", "Audi Q7", "Audi Q8", "Audi TT"],
-    "car":[
+var cars = {
+    "name": ["Audi e-tron", "Audi A1", "Audi A4", "Audi A5", "Audi A6", "Audi A7", "Audi A8", "Audi Q2", "Audi Q3", "Audi Q5", "Audi Q7", "Audi Q8", "Audi TT"],
+    "car": [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
@@ -16,46 +16,119 @@ var data = {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
     ]
 }
-    
 
-//loop for cars
-var eCar = document.getElementsByClassName("glider")[0];
-for (let i = 1; i <= 13; i++) {
-    eCar.innerHTML +=
-        "<a id='data1' onclick='closing(" + i + ")' data-toggle='collapse' data-target='#car" + i + "' aria-controls='car" + i + "'\
-                ria-expanded='false' data-parent='#data'>\
-                <img id='pic"+ i + "' src='./Image+Site/first/2." + i + ".1.png' alt=''><p>" + data["name"][i-1] + "</p></a>";
+var bs = {
+    "name": ["SUV", "Sportback", "Sedan", "Avant", "Coupé", "Sport", "Roadster"],
+    "car": [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus velit, semper non elit sit amet, pretium viverra lacus. Maecenas ornare lorem ac lacus ultricies, vel congue sem accumsan. Curabitur venenatis massa mauris, consectetur tempus mi euismod sit amet. Vestibulum sagittis erat vel nisl euismod, eget fringilla purus iaculis. Suspendisse metus ligula, imperdiet sed mi in, mattis mattis sapien. Vivamus in interdum lorem, nec sodales justo. Curabitur ut sodales ex, ut finibus felis. Sed ultricies diam at sapien egestas, eget tempus purus feugiat. Sed suscipit efficitur ex, at ullamcorper urna ornare nec. Etiam eget cursus odio. Praesent tempor aliquam metus quis semper. Vivamus ligula quam, rhoncus eget enim posuere, blandit auctor dui. In hac habitasse platea dictumst. Aenean sollicitudin lacinia nisi, a efficitur orci rhoncus posuere. Maecenas faucibus ac arcu ut ornare.",
+    ]
 }
 
-//loop for data cars
-var eData = document.getElementById("car");
-for (let i = 1; i <= 13; i++) {
-    eData.innerHTML +=
-        '<div class="collapse border-top mt-3" id="car'+i+'" data-parent="#data">\
-        <Button class="btn btn-dark rounded-circle my-4" data-toggle="collapse" data-target="#car'+i+'">X</Button>\
-        <h5 class="font-weight-bolder">Audi e-tron</h5>\
-        <p>'+ data["car"][i-1] +'</p></div>';
+//loop for cars
+var eCar1 = document.getElementsByClassName("glider1")[0];
+var eCar2 = document.getElementsByClassName("glider2")[0];
+var eData1 = document.getElementById("car1");
+var eData2 = document.getElementById("car2");
 
+for (let i = 1; i <= 13; i++) {
+        eCar1.innerHTML +=
+        "<a id='car"+i+"' class='mr-5' onclick='cclosing(" + i + ")' data-toggle='collapse' data-target='#cdata" + i + "' aria-controls='cdata" + i + "'\
+        ria-expanded='false' data-parent='#slide1'>\
+        <img id='cpic"+ i + "' src='./Image+Site/first/2." + i + ".1.png' alt=''><p>" + cars["name"][i - 1] + "</p></a>";
+        
+        eData1.innerHTML +=
+        '<div id="cdata' + i + '" class="collapse border-top" data-parent="#slide1">\
+        <Button class="btn btn-dark rounded-circle my-4" data-toggle="collapse" data-target="#cdata'+ i + '">X</Button>\
+        <h5 class="font-weight-bolder">' + cars["name"][i - 1] + '</h5>\
+        <p>'+ cars["car"][i - 1] + '</p></div>';
+}
+
+for (let i = 1; i <= 7; i++) {
+    eCar2.innerHTML +=
+        "<a id='body" + i + "' class='mr-5' onclick='bclosing(" + i + ")' data-toggle='collapse' data-target='#bdata" + i + "' aria-controls='bbody" + i + "'\
+    ria-expanded='false' data-parent='#slide2'>\
+    <img id='bpic"+ i + "' src='./Image+Site/first/2." + i + ".1.png' alt=''><p>" + bs["name"][i - 1] + "</p></a>";
+
+    eData2.innerHTML +=
+        '<div  id="bdata' + i + '" class="collapse border-top" data-parent="#slide2">\
+    <Button class="btn btn-dark rounded-circle my-4" data-toggle="collapse" data-target="#bdata'+ i + '">X</Button>\
+    <h5 class="font-weight-bolder">'+ bs["name"][i - 1] + '</h5>\
+    <p>'+ bs["car"][i - 1] + '</p></div>';
 }
 
 //closing function
-function closing(i) {
-    $("#car" + i + "").on("hide.bs.collapse", function () {
-        $("#pic" + i + "").attr('src', './Image+Site/first/2.' + i + '.1.png');
+function cclosing(i) {
+    $("#cdata" + i + "").on("hide.bs.collapse", function () {
+        $("#cpic" + i + "").attr('src', './Image+Site/first/2.' + i + '.1.png');
     });
-    $("#car" + i + "").on("show.bs.collapse", function () {
-        $("#pic" + i + "").attr('src', './Image+Site/first/2.' + i + '.2.png');
+    $("#cdata" + i + "").on("show.bs.collapse", function () {
+        $("#cpic" + i + "").attr('src', './Image+Site/first/2.' + i + '.2.png');
+    });
+}
+function bclosing(i) {
+    $("#bdata" + i + "").on("hide.bs.collapse", function () {
+        $("#bpic" + i + "").attr('src', './Image+Site/first/2.' + i + '.1.png');
+    });
+    $("#bdata" + i + "").on("show.bs.collapse", function () {
+        $("#bpic" + i + "").attr('src', './Image+Site/first/2.' + i + '.2.png');
     });
 }
 
+
 //glider function
-window.addEventListener('load', function () {
-    new Glider(document.querySelector('.glider'), {
-        slidesToShow: 6.5,
-        slidesToScroll: 6,
-        arrows: {
-            prev: '.glider-prev',
-            next: '.glider-next'
-        },
-    });
+var glider1 = new Glider(document.querySelector('.glider1'), {
+    slidesToShow: 6.5,
+    slidesToScroll: 6,
+    arrows: {
+        prev: '.glider-prev1',
+        next: '.glider-next1'
+    },    
+});    
+
+var glider2 = new Glider(document.querySelector('.glider2'), {
+    slidesToShow: 6.5,
+    slidesToScroll: 1,
+    arrows: {
+        prev: '.glider-prev2',
+        next: '.glider-next2'
+    },
+});
+
+
+document.getElementsByClassName('glider-contain2')[0].classList.add("hide");
+
+//button click
+var models = document.getElementById("models");
+var body = document.getElementById("body");
+
+models.addEventListener('click', () => {
+    if (!body.classList.contains('active')) {
+        models.classList.add('active')
+    } else {
+        body.classList.remove('active')
+        models.classList.add('active')
+        document.getElementsByClassName('glider-contain1')[0].classList.remove("hide");
+        document.getElementsByClassName('glider-contain2')[0].classList.add("hide");
+
+    }
 })
+
+
+body.addEventListener('click', () => {
+    if (!models.classList.contains('active')) {
+        body.classList.add('active')
+    } else {
+        models.classList.remove('active')
+        body.classList.add('active')
+        document.getElementsByClassName('glider-contain2')[0].classList.remove("hide");
+        document.getElementsByClassName('glider-contain1')[0].classList.add("hide");
+
+    }
+})
+
