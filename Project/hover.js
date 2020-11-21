a6e87f2c27989410c9c936e1531af1b7a0dda6fa
sedthanun <62070212@kmitl.ac.th>
2020-11-22 01:01:11 +0700

    Upgrade Navbar/update pic

---
+++ Project/hover.js
@@ -0,0 +1,45 @@
+function a4avant(){
+    document.getElementById("pic").setAttribute("src", "Image+Site/Navbar/a4_2.png");
+    document.getElementById("car_model").innerHTML = "A4 Avant >";
+    document.getElementById("car_spec").innerHTML = "Sporty design, high functionality. <br>The Audi A4 Avant.";
+   }
+   function a4sedan(){
+    document.getElementById("pic").setAttribute("src", "Image+Site/Navbar/a4.png");
+    document.getElementById("car_model").innerHTML = "A4 Sedan >";
+    document.getElementById("car_spec").innerHTML = "A statement of sportiness. <br> The Audi A4 Sedan.";
+   }
+   function rs4(){
+    document.getElementById("pic").setAttribute("src", "Image+Site/Navbar/a4_3.png");
+    document.getElementById("car_model").innerHTML = "RS 4 Avant >";
+    document.getElementById("car_spec").innerHTML = "Function meets high performance. <br> The Audi RS 4 Avant.";
+   }
+   function a5coupe(){
+    document.getElementById("pic2").setAttribute("src", "Image+Site/Navbar/a5.png");
+    document.getElementById("car_model2").innerHTML = "A5 Coupe >";
+    document.getElementById("car_spec2").innerHTML = "Object of attention? It's already on the road. <br>The Audi A5 Coupé.";
+   }
+   function a5sportback(){
+    document.getElementById("pic2").setAttribute("src", "Image+Site/Navbar/a5_1.png");
+    document.getElementById("car_model2").innerHTML = "A5 Sportback >";
+    document.getElementById("car_spec2").innerHTML = "Sporty and suitable for families. <br> The Audi A5 Sportback.";
+   }
+   function a6sedan(){
+    document.getElementById("pic3").setAttribute("src", "Image+Site/Navbar/a6.png");
+    document.getElementById("car_model3").innerHTML = "A6 Sedan >";
+    document.getElementById("car_spec3").innerHTML = "Progress is not just felt. It is experienced. <br> The new A6 Sedan.";
+   }
+   function a6avant(){
+    document.getElementById("pic3").setAttribute("src", "Image+Site/Navbar/a6_2.png");
+    document.getElementById("car_model3").innerHTML = "A6 Avant >";
+    document.getElementById("car_spec3").innerHTML = "Determinedly many-sided. The new Audi A6 Avant.";
+   }
+   function q3(){
+    document.getElementById("pic4").setAttribute("src", "Image+Site/Navbar/q3.png");
+    document.getElementById("car_model4").innerHTML = "Q3 >";
+    document.getElementById("car_spec4").innerHTML = "It’s Happening The new Audi Q3.";
+   }
+   function q3sportback(){
+    document.getElementById("pic4").setAttribute("src", "Image+Site/Navbar/q3_2.png");
+    document.getElementById("car_model4").innerHTML = "Q3 Sportback >";
+    document.getElementById("car_spec4").innerHTML = "Extroverted SUV coupé. The Audi Q3 Sportback.";
+   }
