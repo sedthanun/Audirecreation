b784e37760e19b01e6c60001816a707e082e5e03
Patit Kaewjinda <riew10580@gmail.com>
2020-11-25 11:44:56 +0700

    Update navbar path

--- Project/hover.js
+++ Project/hover.js
@@ -1,115 +1,115 @@
 function a4avant(){
     document.getElementById("pic").setAttribute("src", "Image+Site/Navbar/a4_2.png");
     document.getElementById("car_model").innerHTML = "A4 Avant >";
-    document.getElementById("car_model").setAttribute("href", "#aaa");
+    document.getElementById("car_model").setAttribute("href", "a4_avant.html");
     document.getElementById("car_spec").innerHTML = "Sporty design, high functionality. <br>The Audi A4 Avant.";
    }
    function a4sedan(){
     document.getElementById("pic").setAttribute("src", "Image+Site/Navbar/a4.png");
     document.getElementById("car_model").innerHTML = "A4 Sedan >";
-    document.getElementById("car_model").setAttribute("href", "#aaa");
+    document.getElementById("car_model").setAttribute("href", "a4_sedan.html");
     document.getElementById("car_spec").innerHTML = "A statement of sportiness. <br> The Audi A4 Sedan.";
    }
    function rs4(){
     document.getElementById("pic").setAttribute("src", "Image+Site/Navbar/a4_3.png");
     document.getElementById("car_model").innerHTML = "RS 4 Avant >";
-    document.getElementById("car_model").setAttribute("href", "#aaa");
+    document.getElementById("car_model").setAttribute("href", "rs_a4.html");
     document.getElementById("car_spec").innerHTML = "Function meets high performance. <br> The Audi RS 4 Avant.";
    }
    function a5coupe(){
     document.getElementById("pic2").setAttribute("src", "Image+Site/Navbar/a5.png");
     document.getElementById("car_model2").innerHTML = "A5 Coupe >";
-    document.getElementById("car_model2").setAttribute("href", "Image+Site/A5/A5 Coupé/a5.html");
+    document.getElementById("car_model2").setAttribute("href", "a5coupe.html");
     document.getElementById("car_spec2").innerHTML = "Object of attention? It's already on the road. <br>The Audi A5 Coupé.";
    }
    function a5sportback(){
     document.getElementById("pic2").setAttribute("src", "Image+Site/Navbar/a5_1.png");
     document.getElementById("car_model2").innerHTML = "A5 Sportback >";
-    document.getElementById("car_model2").setAttribute("href", "#aaa");
+    document.getElementById("car_model2").setAttribute("href", "a5_sportback.html");
     document.getElementById("car_spec2").innerHTML = "Sporty and suitable for families. <br> The Audi A5 Sportback.";
    }
    function a6sedan(){
     document.getElementById("pic3").setAttribute("src", "Image+Site/Navbar/a6.png");
     document.getElementById("car_model3").innerHTML = "A6 Sedan >";
-    document.getElementById("car_model3").setAttribute("href", "Image+Site/A6/A6 Sedan/a6_sedan.html");
+    document.getElementById("car_model3").setAttribute("href", "a6_sedan.html");
     document.getElementById("car_spec3").innerHTML = "Progress is not just felt. It is experienced. <br> The new A6 Sedan.";
    }
    function a6avant(){
     document.getElementById("pic3").setAttribute("src", "Image+Site/Navbar/a6_2.png");
     document.getElementById("car_model3").innerHTML = "A6 Avant >";
-    document.getElementById("car_model3").setAttribute("href", "#aaa");
+    document.getElementById("car_model3").setAttribute("href", "a6_avant.html");
     document.getElementById("car_spec3").innerHTML = "Determinedly many-sided. The new Audi A6 Avant.";
    }
    function q3(){
     document.getElementById("pic4").setAttribute("src", "Image+Site/Navbar/q3.png");
     document.getElementById("car_model4").innerHTML = "Q3 >";
-    document.getElementById("car_model4").setAttribute("href", "#aaa");
+    document.getElementById("car_model4").setAttribute("href", "Q3.html");
     document.getElementById("car_spec4").innerHTML = "It’s Happening The new Audi Q3.";
    }
    function q3sportback(){
     document.getElementById("pic4").setAttribute("src", "Image+Site/Navbar/q3_2.png");
     document.getElementById("car_model4").innerHTML = "Q3 Sportback >";
-    document.getElementById("car_model4").setAttribute("href", "#aaa");
+    document.getElementById("car_model4").setAttribute("href", "Q3_Sportback.html");
     document.getElementById("car_spec4").innerHTML = "Extroverted SUV coupé. The Audi Q3 Sportback.";
    }
    function thenewq8(){
     document.getElementById("pic5").setAttribute("src", "Image+Site/Navbar/q8.png");
     document.getElementById("car_model5").innerHTML = "The new Q8 >";
-    document.getElementById("car_model5").setAttribute("href", "Image+Site/Q8/New Q8/q8.html");
+    document.getElementById("car_model5").setAttribute("href", "q8.html");
     document.getElementById("car_spec5").innerHTML = "The new face of the Q family.";
    }
    function q8(){
     document.getElementById("pic5").setAttribute("src", "Image+Site/Navbar/q8_2.png");
     document.getElementById("car_model5").innerHTML = "RS Q8 >";
-    document.getElementById("car_model5").setAttribute("href", "#aaa");
+    document.getElementById("car_model5").setAttribute("href", "rs_q8.html");
     document.getElementById("car_spec5").innerHTML = "Performance in a new dimension. <br> The Audi RS Q8.";
    }
    function tt1(){
     document.getElementById("pic6").setAttribute("src", "Image+Site/Navbar/tt.png");
     document.getElementById("car_model6").innerHTML = "TT Coupé >";
-    document.getElementById("car_model6").setAttribute("href", "#aaa");
+    document.getElementById("car_model6").setAttribute("href", "tt_coupe.html");
     document.getElementById("car_spec6").innerHTML = "The Audi TT Coupé. Pure emotion packaged <br> in form and function. Pure consistency in <br> quality and efficiency. Pure sportsmanship <br> brought to the road.";
    }
    function tt2(){
     document.getElementById("pic6").setAttribute("src", "Image+Site/Navbar/tt_2.png");
     document.getElementById("car_model6").innerHTML = "TT Roadster >";
-    document.getElementById("car_model6").setAttribute("href", "#aaa");
+    document.getElementById("car_model6").setAttribute("href", "tt_roadster.html");
     document.getElementById("car_spec6").innerHTML = "Universal talent. The Audi TT Roadster.";
    }
    function tt3(){
     document.getElementById("pic6").setAttribute("src", "Image+Site/Navbar/tt_3.png");
     document.getElementById("car_model6").innerHTML = "TT RS Coupé  >";
-    document.getElementById("car_model6").setAttribute("href", "#aaa");
+    document.getElementById("car_model6").setAttribute("href", "tt_rs_coupe.html");
     document.getElementById("car_spec6").innerHTML = "Ultimate sportiness. The Audi TT RS Coupé.";
    }
    function rs1(){
     document.getElementById("pic7").setAttribute("src", "Image+Site/Navbar/rs.png");
     document.getElementById("car_model7").innerHTML = "RS 4 Avant >";
-    document.getElementById("car_model7").setAttribute("href", "#aaa");
+    document.getElementById("car_model7").setAttribute("href", "rs_a4.html");
     document.getElementById("car_spec7").innerHTML = "Function meets high performance. The Audi RS 4 Avant.";
    }
    function rs2(){
     document.getElementById("pic7").setAttribute("src", "Image+Site/Navbar/q8.png");
     document.getElementById("car_model7").innerHTML = "RS Q8 >";
-    document.getElementById("car_model7").setAttribute("href", "#aaa");
+    document.getElementById("car_model7").setAttribute("href", "rs_q8.html");
     document.getElementById("car_spec7").innerHTML = "Performance in a new dimension. <br> The Audi RS Q8.";
    }
    function rs3(){
     document.getElementById("pic7").setAttribute("src", "Image+Site/Navbar/tt_3.png");
     document.getElementById("car_model7").innerHTML = "TT RS Coupé  >";
-    document.getElementById("car_model7").setAttribute("href", "#aaa");
+    document.getElementById("car_model7").setAttribute("href", "tt_rs_coupe.html");
     document.getElementById("car_spec7").innerHTML = "Ultimate sportiness. The Audi TT RS Coupé.";
    }
    function e1(){
     document.getElementById("pic8").setAttribute("src", "Image+Site/Navbar/etron.png");
     document.getElementById("car_model8").innerHTML = "Audi e-tron >";
-    document.getElementById("car_model8").setAttribute("href", "#aaa");
+    document.getElementById("car_model8").setAttribute("href", "etron.html");
     document.getElementById("car_spec8").innerHTML = "Shaping the future. The Audi e-tron.";
    }
    function e2(){
     document.getElementById("pic8").setAttribute("src", "Image+Site/Navbar/etron2.png");
     document.getElementById("car_model8").innerHTML = "Audi e-tron Sportback  >";
-    document.getElementById("car_model8").setAttribute("href", "#aaa");
+    document.getElementById("car_model8").setAttribute("href", "etron_sport.html");
     document.getElementById("car_spec8").innerHTML = "Design, reinterpreted. The Audi e-tron Sportback.";
    }
 
